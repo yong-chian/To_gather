a94f05df70657684332f1fdf97579794f47143cb
YongChian Teo <yong-chian@hotmail.com>
2023-03-18 00:56:12 +0800

    changes to activity.rb

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,10 +2,10 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_one :meeting_location
 
-  validates :name,
-  validates :description,
-  validates :price,
-  validates :max_capacity,
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :max_capacity, presence: true
   validates :meeting_location, presence: true
   validates :name, uniqueness: true
 end
