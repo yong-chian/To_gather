5f83032e4fef8cf9418c575919bba968125a0e6a
YongChian Teo <yong-chian@hotmail.com>
2023-03-22 21:02:20 +0800

    added activity view pages

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -26,6 +26,22 @@ class ActivitiesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @activity.update(activity_params)
+      redirect_to activity_path(@experience), notice: "Activity was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to activities_path, notice: "Activity was successfully destroyed."
+  end
+
   private
 
   def activity_params
