a8c00f725daa4cd271cc32b46333c208a5fab79e
YongChian Teo <yong-chian@hotmail.com>
2023-03-31 02:42:49 +0800

    participant review model in progress

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  # belongs_to :activity
+  belongs_to :activity
   # has_one :host_review
   # has_one :participant_review
   belongs_to :availability
--- app/models/participant_review.rb
+++ app/models/participant_review.rb
@@ -4,6 +4,6 @@ class ParticipantReview < ApplicationRecord
 
   validates :content, presence: true
   validates :user_name, presence: true
-  validates :rating, presence: true, numericality: {
+  validates :activity_rating, presence: true, numericality: {
     only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
