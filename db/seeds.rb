6fbb32c21b873e9bfbd5ea457431e09371c68abb
Evonne Li <evonnelyg@gmail.com>
2023-03-31 15:37:24 +0800

    added fa icons and made changes to .erb for interest icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,27 +46,28 @@ test_user18 = User.create(email: "test17@gmail.com", password: "123456q", first_
 test_user19 = User.create(email: "test18@gmail.com", password: "123456r", first_name: "Jared", last_name: "Koh", phone_no: "12345678")
 
 # TODO: Created interests
-Interest.create!(name: "Arts and Crafts", icon:'<i class='"fas fa-graduation-cap'" style="font-size:48px;color:red"></i>)
-Interest.create!(name: "Book Clubs", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Cultural Celebrations", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Charity Drives", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Cooking", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Education", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Fund-raising Events", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Gardening", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Technology", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Music", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Sports & Wellness", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "SG Heritage Tours", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Social Clubs", icon:'<i class="fa-solid fa-tv"></i>')
-Interest.create!(name: "Professional Development", icon:'<i class="fa-solid fa-tv"></i>')
+# TODO: Created interests
+Interest.create!(name: "Arts and Crafts", icon:'<i class="fas fa-palette"></i>')
+Interest.create!(name: "Book Clubs", icon:'<i class="fas fa-book-open"></i>')
+Interest.create!(name: "Cultural Celebrations", icon:'<i class="fas fa-theater-masks"></i>')
+Interest.create!(name: "Charity Drives", icon: '<i class="fas fa-hand-holding-heart"></i>')
+Interest.create!(name: "Education", icon:'<i class="fas fa-graduation-cap"></i>')
+Interest.create!(name: "Entertainment", icon:'<i class="fas fa-gamepad"></i>')
+Interest.create!(name: "Food", icon:'<i class="fas fa-hamburger"></i>')
+Interest.create!(name: "Gardening", icon:'<i class="fas fa-seedling"></i>')
+Interest.create!(name: "Technology", icon:'<i class="fas fa-laptop-code"></i>')
+Interest.create!(name: "Music", icon:'<i class="fas fa-music"></i>')
+Interest.create!(name: "Sports & Wellness", icon:'<i class="fas fa-biking"></i>')
+Interest.create!(name: "SG Heritage Tours", icon:'<i class="fas fa-binoculars"></i>')
+Interest.create!(name: "Social Clubs", icon:'<i class="fas fa-user-friends"></i>')
+Interest.create!(name: "Professional Development", icon:'<i class="fas fa-chalkboard-teacher"></i>')
 
 p "Created #{Interest.count} interests"
 
 # TODO: Created activities
 # Activities for H2 - What's Popular Near You
 
-gimage1_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720473/photo-1660099789632-ed5fae6189af_hwqji8.jpg
+image1_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720473/photo-1660099789632-ed5fae6189af_hwqji8.jpg"
 activity_1 = Activity.create(
   name: "Mahjong with Pros",
   description: "Learn and play Mahjong with experienced players for all levels." ,
@@ -77,19 +78,24 @@ activity_1 = Activity.create(
   policies: "Please inform us for rescheduling in the event you are unwell",
   user: test_user
 )
+
 file = URI.open(image1_url)
 activity_1.photos.attach(io: file, filename: "#{activity_1.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_1.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_1.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_1.save!
 
 image2_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/e_art:refresh/v1679715220/image-asset_m0sp3q.jpg"
@@ -103,19 +109,24 @@ activity_2 = Activity.create(
   policies: "Nil",
   user: test_user2
 )
+
 file = URI.open(image2_url)
 activity_2.photos.attach(io: file, filename: "#{activity_2.name}.png", content_type: "image/png")
 availabilities = []
 date = Date.today
+
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_2.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_2.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_2.save!
 
 
@@ -130,24 +141,29 @@ activity_3 = Activity.create(
   policies: "Nil",
   user: test_user3,
 )
+
 file = URI.open(image3_url)
 activity_3.photos.attach(io: file, filename: "#{activity_3.name}.png", content_type: "image/png")
 availabilities = []
 date = Date.today
+
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_3.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_3.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_3.save!
 
 image4_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679719646/Peranakan-dishes_rohnte.jpg"
 activity_4 = Activity.create(
-  name: "Peranankan Cooking Course",
+  name: "Peranakan Cooking Course",
   description: "Learn to cook 3 traditional Peranakan dishes with authentic ingredients in our cozy home. The dishes include Ayam Buah Keluak, Babi Pongteh and Kueh Lapis.",
   price: 100,
   max_capacity: 5,
@@ -156,19 +172,24 @@ activity_4 = Activity.create(
   policies: "Nil",
   user: test_user4
 )
+
 file = URI.open(image4_url)
 activity_4.photos.attach(io: file, filename: "#{activity_4.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_4.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_4.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_4.save!
 
 image5_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679719737/bc22ea20-84a0-11ea-bdf5-3ea9c00cb2c0_amgdrd.jpg"
@@ -184,17 +205,21 @@ activity_5 = Activity.create(
 )
 file = URI.open(image5_url)
 activity_5.photos.attach(io: file, filename: "#{activity_5.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_5.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_5.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_5.save!
 
 image6_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679719911/E417767AE8C6CC63B601C2F474916F4E_f5d9j7.jpg"
@@ -208,19 +233,24 @@ activity_6 = Activity.create(
   policies: "Nil",
   user: test_user6
 )
+
 file = URI.open(image6_url)
 activity_6.photos.attach(io: file, filename: "#{activity_6.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_6.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_6.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_6.save!
 
 image7_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679719984/photo-1633869284807-d35bbe7d8719_tibjma.jpg"
@@ -237,17 +267,21 @@ activity_7 = Activity.create(
 )
 file = URI.open(image7_url)
 activity_7.photos.attach(io: file, filename: "#{activity_7.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_7.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_7.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_7.save!
 
 image8_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720766/photo-1679110667877-408593fab0f6_glolqa.jpg"
@@ -261,14 +295,17 @@ activity_8 = Activity.create(
   policies: "Nil",
   user: test_user8
 )
+
 file = URI.open(image8_url)
 activity_8.photos.attach(io: file, filename: "#{activity_8.name}.png", content_type: "image/png")
 availabilities = []
 date = Date.today
+
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
+
 availabilities.map { |time| activity_8.availabilities.build(
   start_time: time,
   end_time: time
@@ -287,19 +324,24 @@ activity_9 = Activity.create(
   policies: "Nil",
   user: test_user9
 )
+
 file = URI.open(image9_url)
 activity_9.photos.attach(io: file, filename: "#{activity_9.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_9.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_9.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_9.save!
 
 # Activities for H2 - Browse Other Options
@@ -316,17 +358,21 @@ activity_10 = Activity.create(
 )
 file = URI.open(image10_url)
 activity_10.photos.attach(io: file, filename: "#{activity_10.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_10.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_10.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_10.save!
 
 image11_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720325/f7f8e635697ba19e9b09f926cad92d4f--bird-watching-singapore_czv1tz.jpg"
@@ -343,17 +389,21 @@ activity_11 = Activity.create(
 )
 file = URI.open(image11_url)
 activity_11.photos.attach(io: file, filename: "#{activity_11.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_11.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_11.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_11.save!
 
 image12_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/v1679717906/9bda435e-03e5-4f8a-beab-9b4193fefc13_eqik7p.jpg"
@@ -370,17 +420,21 @@ activity_12 = Activity.create(
 )
 file = URI.open(image12_url)
 activity_12.photos.attach(io: file, filename: "#{activity_12.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_12.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_12.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_12.save!
 
 image13_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720573/EPC-0350-2rec-web_lyjdge.webp"
@@ -397,17 +451,21 @@ activity_13 = Activity.create(
 )
 file = URI.open(image13_url)
 activity_13.photos.attach(io: file, filename: "#{activity_13.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_13.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_13.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_13.save!
 
 image14_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679719737/bc22ea20-84a0-11ea-bdf5-3ea9c00cb2c0_amgdrd.jpg"
@@ -421,19 +479,24 @@ activity_14 = Activity.create(
   policies: "Please inform us for rescheduling in the event you are unwell",
   user: test_user14
 )
+
 file = URI.open(image14_url)
 activity_14.photos.attach(io: file, filename: "#{activity_14.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_14.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_14.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_14.save!
 
 image15_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720974/photo-1675495277087-10598bf7bcd1_wp0qrh.jpg"
@@ -450,17 +513,21 @@ activity_15 = Activity.create(
 )
 file = URI.open(image15_url)
 activity_15.photos.attach(io: file, filename: "#{activity_15.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_15.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_15.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_15.save!
 
 image16_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/v1679721433/meg-wagener-vuXTB1lR3AY-unsplash_xldwsn.jpg"
@@ -477,17 +544,21 @@ activity_16 = Activity.create(
 )
 file = URI.open(image16_url)
 activity_16.photos.attach(io: file, filename: "#{activity_16.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_16.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_16.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_16.save!
 
 image17_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679714826/oleg-ivanov-iB_h6EFRiKY-unsplash_vkxphf.jpg"
@@ -504,17 +575,21 @@ activity_17 = Activity.create(
 )
 file = URI.open(image17_url)
 activity_17.photos.attach(io: file, filename: "#{activity_17.name}.png", content_type: "image/png")
+
 availabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_17.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_17.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
+end
+
 activity_17.save!
 
 image18_url = "https://res.cloudinary.com/ddk4z9ypx/image/upload/c_scale,e_art:refresh,w_1170/v1679720205/easy-thai-recipes-for-beginners_unl1u4.jpg"
@@ -529,32 +604,22 @@ activity_18 = Activity.create(
   policies: "Please inform us for rescheduling in the event you are unwell",
   user: test_user18
 )
-  file = URI.open(image18_url)
+file = URI.open(image18_url)
 activity_18.photos.attach(io: file, filename: "#{activity_18.name}.png", content_type: "image/png")
-availabilities = []
+aavailabilities = []
 date = Date.today
 30.times do
   availabilities << date.strftime("%Y-%m-%d")
   date += 1.day
 end
-availabilities.map { |time| activity_18.availabilities.build(
-  start_time: time,
-  end_time: time
+
+availabilities.each do |time|
+  activity_18.availabilities.build(
+    start_time: time,
+    end_time: time
   )
-}
-activity_18.save!
-activity_18.validate!
-availabilities = []
-date = Date.today
-30.times do
-  availabilities << date.strftime("%Y-%m-%d")
-  date += 1.day
 end
-availabilities.map { |time| activity_18.availabilities.build(
-  start_time: time,
-  end_time: time
-  )
-}
+
 activity_18.save!
 
 p "Created #{Activity.count} activities"
