aeb477f21f5b692bca76f7d8cf942317ece2d035
YongChian Teo <yong-chian@hotmail.com>
2023-04-02 21:06:52 +0800

    add latitude and longitude to activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -15,6 +15,8 @@ class Activity < ApplicationRecord
   has_many :faqs
   has_many :bookings, dependent: :destroy
   has_many :participant_reviews, through: :bookings
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
   include PgSearch::Model
 
