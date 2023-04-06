2290fe149c81c7ec9c9c6b119e16f4628e9e4d09
YongChian Teo <yong-chian@hotmail.com>
2023-04-07 02:25:54 +0800

    fix order controller, add booking reference to orders, fix bookings page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,39 @@
 class OrdersController < ApplicationController
   def create
-    raise
-    activity = Activity.find(params[:activity_id])
-    order = Order.create!(activity: activity, activity_sku: activity.sku, amount: activity.price, state: 'pending', user: current_user)
+    @booking = Booking.find(params[:booking_id])
+    @activity = Activity.find(params[:activity_id])
+    order = Order.create!(booking: @booking,
+                          activity: @activity,
+                          activity_sku: @booking.activity.sku,
+                          amount: @activity.price,
+                          state: 'pending',
+                          user: current_user)
     authorize order
     session = Stripe::Checkout::Session.create(
-        line_items: [{
-            price_data: {
-                currency: 'sgd',
-                unit_amount: activity.price_cents,
-                product_data: {
-                    name: activity.name,
-                }
-            },
-            quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: order_url(order),
-        cancel_url: order_url(order)
+      line_items: [{
+        price_data: {
+          currency: 'sgd',
+          unit_amount: @activity.price_cents,
+          product_data: {
+            name: @activity.name
+          }
+        },
+        quantity: 1
+      }],
+      mode: 'payment',
+      success_url: order_url(order),
+      cancel_url: order_url(order)
     )
-
     order.update(checkout_session_id: session.id)
     redirect_to new_order_payment_path(order)
   end
 
   def show
-      # success_url: order_url(order)
-      @order = current_user.orders.find(params[:id])
-      authorize (@order)
+    # success_url: order_url(order)
+    @order = current_user.orders.find(params[:id])
+    @booking = @order.booking
+    @booking.status = "Paid & Pending"
+    @booking.save!
+    authorize(@order)
   end
 end
