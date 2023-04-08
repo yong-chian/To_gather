23592205f65c10413167ced39c7236450edd8596
amir haikel <amir.haikel@gmail.com>
2023-04-08 10:06:10 +0800

    Added Animation Effect stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import ActivityFlatpickrController from "./activity_flatpickr_controller"
 application.register("activity-flatpickr", ActivityFlatpickrController)
 
+import AnimationController from "./animation_controller"
+application.register("animation", AnimationController)
+
 import ChatroomSubscriptionController from "./chatroom_subscription_controller"
 application.register("chatroom-subscription", ChatroomSubscriptionController)
 
